f6e1377cc8c6af8508708f5743863e94cff5e2fc
Garvel31 <alexp0beda88@gmail.com>
2021-10-28 17:19:31 +0300

    добавил решение по 2му пункту

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,9 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
+        System.out.println(args[0]);
         try {
             System.out.println(StringUtils.upperCase(args[0]));
         } catch (ArrayIndexOutOfBoundsException e){
